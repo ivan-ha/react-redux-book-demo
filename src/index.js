ba1a2c5f5c6b4826a0f3a527f70a2b51d2fcc130
Ivan HA <hangolam@gmail.com>
2017-05-11 22:18:43 +0800

    redux implementation on book-list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 import App from './components/app'
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+const store = applyMiddleware()(createStore);
+const AppContainer = () => {
+    return (
+        <Provider store={store(reducers)}>
+            <App />
+        </Provider>
+    )
+}
+
+ReactDOM.render(<AppContainer />, document.querySelector('.container'));
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: "React"},
+        {title: "Flux"},
+        {title: "Redux"},
+        {title: "Javascript"}
+    ];
+}
